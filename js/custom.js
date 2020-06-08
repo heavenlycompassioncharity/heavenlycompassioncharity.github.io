cdd400597906310e3e597dbd0c401846dc8ffb02
reynold <reynold.da1@iiitmk.ac.in>
2020-06-09 00:23:54 +0530

    update volumteers

--- js/custom.js
+++ js/custom.js
@@ -82,3 +82,22 @@ mailChimp();
 
 
 }(jQuery));
+
+
+
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+});
+// $('.play').on('click',function(){
+//     owl.trigger('play.owl.autoplay',[1000])
+// })
+// $('.stop').on('click',function(){
+//     owl.trigger('stop.owl.autoplay')
+// })
